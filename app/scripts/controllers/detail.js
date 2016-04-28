e3d6b2c334573e2d956c1342617d7424a4fa82bd
scarletmu@126.com <scarletmu@126.com>
2016-04-28 17:46:48 +0800

    missing #

--- app/scripts/controllers/detail.js
+++ app/scripts/controllers/detail.js
@@ -26,7 +26,7 @@ angular.module('ScarletBlog').controller('DetailCtrl',function($scope,$q,$stateP
   };
 
   $scope.Share = function(){
-    $scope.nowUrl = 'https://scarletmu.space'+$location.url();
+    $scope.nowUrl = 'https://scarletmu.space/#'+$location.url();
       (function(s,d,e){try{}catch(e){}var f='http://v.t.sina.com.cn/share/share.php?',u=$scope.nowUrl,p=['url=',e(u),'&title=',e($scope.topicData.Title),'&pic=',e($scope.topicData.Thumbnail)].join('');
         function a(){if(!window.open([f,p].join(''),'mb',['toolbar=0,status=0,resizable=1,width=620,height=450,left=',(s.width-620)/2,',top=',(s.height-450)/2].join('')))u.href=[f,p].join('');};
         if(/Firefox/.test(navigator.userAgent)){setTimeout(a,0)}else{a()}})(screen,document,encodeURIComponent);
